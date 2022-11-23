0e055bc5ededee553584750ffc1d98cd48811008
Fabricio-Veloso <fabraccho@gmail.com>
2022-11-23 11:30:57 -0300

    escrita de registros e buscas por nome pelo arquivo indexador concluído!

--- paratestarfunções.c
+++ paratestarfunções.c
@@ -9,6 +9,7 @@ struct archive_index
 {
     char *file_name;
     long int adress;
+   struct archive_index *next;
 };
 
 //estrutura de usuário
@@ -26,31 +27,32 @@ struct txt
     char *name; 
 };
 
-//troca nome de estrutura por arquivo
+// troca nome de estrutura por arquivo
 typedef struct archive_index a_index;
 
-    
-//trocar nome da estrutura para usuário
+// trocar nome da estrutura para usuário
 typedef struct user usuario;
 
-//troca nome da estrutura por txt.
+// troca nome da estrutura por txt.
 typedef struct txt txt;
 
+// Função fwrite adaptada para arquivos idexadores.
+int adaptative_fwrite_index(a_index variavel, FILE *file);
 
-//Função fwrite adaptada para arquivos idexadores.
-int adaptative_fwrite_index( a_index variavel,FILE *file);
+// função fwrite para arquivos de usuário.
+int adaptative_fwrite_user(usuario variavel, FILE *file_arquivo);
 
-//função fwrite para arquivos de usuário.
-int adaptative_fwrite_user( usuario variavel,FILE *file_arquivo);
+// Função fwrite adaptada para textos.
+int adaptative_fwrite_txt(txt variavel, FILE *file_arquivo);
 
-//Função fwrite adaptada para textos.
-int adaptative_fwrite_txt( txt variavel,FILE *file_arquivo);
+// função para encontrar arquivos indexadores por nome
+long int Acha_index(char *nomearquivo, a_index arquivo, FILE *pfile);
 
-//função para encontrar arquivos indexadores por nome
-long int adaptative_fread_index(char *nomearquivo, a_index arquivo, FILE *pfile);
-
-//função para checar arrays.
+// função para checar arrays.
 int check_arrays(char *array1, char *array2);
+
+//printa arrays
+void print_array(char *entrada);
 
 int main(void)
 {
@@ -60,15 +62,15 @@ int main(void)
     printf("\np1\n");
 
     //cria um arquivo do tipo txt(texto) e o prenche com arays de char
-    txt papel;
-    papel.name = "primeiro arquivo de texto";
-    papel.txt = "produção textual de teste";
+    txt arquivo_de_texto;
+    arquivo_de_texto.name = "primeiro arquivo de texto";
+    arquivo_de_texto.txt = "produção textual de teste";
     
      //cria um arquivo do tipo usuário e o prenche com dados de usuário
-    usuario teste;
-    teste.senha = "senha teste";
-    teste.token = 1;
-    teste.user_name = "Fabricio";
+    usuario usuario_teste;
+    usuario_teste.senha = "senha teste";
+    usuario_teste.token = 1;
+    usuario_teste.user_name = "Fabricio";
 
 //cria variáveis para receber indereços que são colocados no arquivo indexador 
     long int adress_a;
@@ -80,7 +82,7 @@ int main(void)
    printf("\np2\n");
     
     // caso não consiga abrir o arquivo de teste retona mensagem, abre aquivo de teste
-    if((arquivo_root = fopen("teste.txt","wb")) == NULL )
+    if((arquivo_root = fopen("teste_index.txt","wb")) == NULL )
     {
         printf("O arquivo não pode ser aberto");
     }
@@ -88,11 +90,11 @@ int main(void)
     {
         //usa afunção fteel para armazenar endereço aonde serão escritos os dados e então os escreve no arquivo teste
         adress_a = ftell(arquivo_root);
-        fwrite(&teste,sizeof(usuario),1,arquivo_root);
+        fwrite(&usuario_teste,sizeof(usuario),1,arquivo_root);
        
 
         adress_b = ftell(arquivo_root);
-        fwrite(&papel,sizeof(txt),1,arquivo_root);
+        fwrite(&arquivo_de_texto,sizeof(txt),1,arquivo_root);
         
 
         //após preencher fecha arquivo
@@ -102,7 +104,7 @@ int main(void)
     printf("\np3\n");
 
     //abre arquivo indexador para gravar nome e endereço dos arquivos tipo indexador que se referem ao arquivos escritos no arquivo teste.
-    if((arquivo_index = fopen("teste.txt","wb")) == NULL )
+    if((arquivo_index = fopen("teste_index.txt","wb")) == NULL )
     {
         printf("O arquivo não pode ser aberto");
     }
@@ -113,21 +115,26 @@ int main(void)
         primeiro_arquivo.file_name = "Fabricio";
         segundo_arquivo.adress = adress_b;
         segundo_arquivo.file_name = "primeiro texto de fabricio";
-        //chama as funções de escrita para escrevelos no arquivo indexador
+        primeiro_arquivo.next = &segundo_arquivo;
+        segundo_arquivo.next = NULL;
+        //chama as funções de escrita para escreve-los no arquivo indexador
         adaptative_fwrite_index(primeiro_arquivo,arquivo_index);
         printf("\np3,5\n");
         adaptative_fwrite_index(segundo_arquivo,arquivo_index);
-        
+
         //printf de teste
         printf("\np3,6\n");
         
-        char *nome_teste = "Fabricio";
-        adaptative_fread_index(nome_teste,primeiro_arquivo,arquivo_index);  
+        char *nome_teste = "primeiro texto de fabricio";
+        printf("\np3,7\n");
         //fechar arquivo indexador
         fclose(arquivo_index);
+        printf("\np300\n");
+        printf("%ld"),(Acha_index(nome_teste,primeiro_arquivo,arquivo_index));  
+        
         
     }
-    printf("\np4\n");
+    printf("\np5\n");
 }
 
 
@@ -149,38 +156,53 @@ int check_arrays(char *array1, char *array2)
     return 1;
 }
 
-
-long int adaptative_fread_index(char *nomearquivo, a_index arquivo, FILE *pfile)
+void print_array(char *entrada)
 {
-    typedef struct archive_index a_index;
+    int i = 0;
+    while( entrada[i] != '\0')
+    {
+        i++;
+        printf("%c",entrada[i-1]);
+    }
+}
 
-    
-
+long int Acha_index(char *nomearquivo, a_index arquivo, FILE *pfile)
+{
+   
 
     //criando função para abrir arquivo indexador, buscar o desejado pelo nome e retornar endereço 
-    if((pfile = fopen("teste.txt","rb")) == NULL )
+    if((pfile = fopen("teste_index.txt","rb")) == NULL )
     {
         printf("O arquivo não pode ser aberto");
     }
-    
-    
+
     else
     {
+        printf("\np3,8\n");
         while (!feof(pfile))
         {
-            (fread(arquivo.file_name,sizeof(a_index),1,pfile));
-            if(check_arrays(arquivo.file_name,nomearquivo) == 0)
+            int cont = 0;
+            printf("\np20\n");
+            (fread(&arquivo,sizeof(a_index),1,pfile));
+            print_array(arquivo.file_name);
+            
+            printf("\np3,9\n");
+            if(check_arrays(arquivo.file_name,nomearquivo) == 1)
             {
+                printf("\np4\n");
                 return arquivo.adress;
             }
-           
+           else
+           {
+            cont++;
+            printf("\np5,1\n");
+            fseek(pfile,cont*sizeof(a_index),SEEK_SET);
+           }
         }
         fclose(pfile);
     }   
-    printf("\np5\n");
+    printf("\np6\n");
 }
-
-
 
 int adaptative_fwrite_index( a_index variavel,FILE *file_arquivo)
 {
